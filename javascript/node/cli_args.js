875a0feec4ab96937a1ea296757a68bdb1fcd84d
NanoDano <nanodano@devdungeon.com>
2015-08-14 17:29:25 -0500

    Updating node file manip, cli and cheerio examples

--- javascript/node/cli_args.js
+++ javascript/node/cli_args.js
@@ -1,8 +1,9 @@
+// Access specific arguments
+console.log(process.argv[0]);
+console.log(process.argv[1]);
+console.log(process.argv[2]);
+
+// Iterate through all arguments
 process.argv.forEach(function(val, index, array) {
   console.log(index + ': ' + val);
 });
-
-/*
-console.log(process.argv[0]);
-console.log(process.argv[1]);
-*/
--- javascript/node/readFile.js
+++
@@ -1,5 +0,0 @@
-fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', function(err, data) {
-    console.log(data);
-});

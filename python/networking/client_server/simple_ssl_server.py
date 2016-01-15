1d0e158a4b313b72a54438d62616be06e5023c7a
NanoDano <nanodano@devdungeon.com>
2016-01-15 11:26:38 -0600

    Adding another simple SSL server example

---
+++ python/networking/client_server/simple_ssl_server.py
@@ -0,0 +1,7 @@
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+
+httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='path/to/localhost.pem', server_side=True)
+httpd.serve_forever()
+

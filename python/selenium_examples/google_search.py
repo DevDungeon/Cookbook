e04b1f532f6e407cd59f55fe28359da6532980ee
NanoDano <nanodano@devdungeon.com>
2019-04-25 11:38:55 -0500

    Update google_search.py

--- python/selenium_examples/google_search.py
+++ python/selenium_examples/google_search.py
@@ -1,17 +1,7 @@
-# Searches Google for something
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 driver = webdriver.Firefox()
-
 driver.get("https://www.google.com")
-
-inputs = driver.find_elements_by_tag_name('input')
-for i in inputs:
-    if i.get_attribute('title') == 'Search':
-        i.send_keys('my search query')
-        i.send_keys(Keys.RETURN)
-        break
-
+driver.find_element_by_xpath('//input[@title="Search"]').send_keys('my search' + Keys.RETURN)
 

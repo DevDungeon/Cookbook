a5341ebdd774471632c069c4b28be6014d218c75
John Leon <nanodano@devdungeon.com>
2021-06-19 17:14:24 -0500

    Updating pyqt5 webengine example

--- python/pyqt5_examples/webview.py
+++ python/pyqt5_examples/webview.py
@@ -4,6 +4,7 @@ import PyQt5
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtWebKitWidgets import QWebView , QWebPage
+from PyQt5 import QtWebEngineWidgets
 from PyQt5.QtWebKit import QWebSettings
 from PyQt5.QtNetwork import *
 import sys
